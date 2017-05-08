cfa81618e63d8cc51bf58d56ff43ec49db80cc6e
Szymon Miedziejko <szymon@mastered.com>
2017-05-08 16:12:11 +0100

    WIP

--- lib/walker.rb
+++ lib/walker.rb
@@ -1,10 +1,17 @@
 class Walker < String
   def replace!
     regexp1 = /<object\s+class="BrightcoveExperience\s+video-player">.+
-              (<param\s+name="playerID"\s+value="(\d+)">).+<\/object>$/ixm
+              (<param\s+name="playerID"\s+value="(\d+)">).+<\/object>/ixm
     regexp2 = /<script.+brightcove\..+<\/script>/im
 
     gsub!(regexp1, 'brightcove-default(\2)')
-    gsub(regexp2, '')
+    gsub!(regexp2, "")
+    fix_css
+  end
+
+  private
+
+  def fix_css
+    gsub(/video-embed-container/, "embed-responsive embed-responsive-16by9")
   end
 end
--- spec/lib/walker_spec.rb
+++ spec/lib/walker_spec.rb
@@ -2,6 +2,27 @@ require 'spec_helper'
 require 'walker'
 
 describe Walker do
+  it 'example without surrounding div' do
+    data = '
+  <object class="BrightcoveExperience video-player">
+  <param name="bgcolor" value="#000000">
+  <param name="width" value="100%">
+  <param name="playerID" value="5397585555001">
+  <param name="playerKey" value="AQ~~,AAACqQLjXwE~,Nq9Z9rk3F20GNgDbGLkEDHbwMyFJR1vx">
+  <param name="isVid" value="true">
+  <param name="isUI" value="true">
+  <param name="dynamicStreaming" value="true">
+  <param name="autoStart" value="false">
+  <param name="includeAPI" value="true">
+  <param name="@videoPlayer" value="5397585555001">
+  <param name="templateLoadHandler" value="onTemplateLoaded">
+  <param name="htmlFallback" value="true">
+  <param name="secureConnections" value="true">
+  <param name="secureHTMLConnections" value="true">
+  </object>'
+    expect(described_class.new(data).replace!).to eq "\n  brightcove-default(5397585555001)"
+  end
+
   it 'example with surrounding div' do
     data = '<!--video-->
 <div class="video-embed-container">
@@ -23,7 +44,7 @@ describe Walker do
   </object>
   <script type="text/javascript">brightcove.createExperiences();</script>
 </div>'
-    expect(described_class.new(data).replace!).to eq "<!--video-->\n<div class=\"video-embed-container\">\n  brightcove-default(5397585555001)\n  \n</div>"
+    expect(described_class.new(data).replace!).to eq "<!--video-->\n<div class=\"embed-responsive embed-responsive-16by9\">\n  brightcove-default(5397585555001)\n  \n</div>"
   end
 
   it 'example without script' do
@@ -46,6 +67,36 @@ describe Walker do
   <param name="secureHTMLConnections" value="true">
   </object>
 </div>'
-    expect(described_class.new(data).replace!).to eq "<!--video-->\n<div class=\"video-embed-container\">\n  brightcove-default(5397585555001)\n</div>"
+    expect(described_class.new(data).replace!).to eq "<!--video-->\n<div class=\"embed-responsive embed-responsive-16by9\">\n  brightcove-default(5397585555001)\n</div>"
+  end
+
+  it 'example with other markdown tag' do
+    data = 'you complete the module.
+
+<!--video-->
+<div class="video-embed-container">
+  <object class="BrightcoveExperience video-player">
+  <param name="bgcolor" value="#000000">
+  <param name="width" value="100%">
+  <param name="playerID" value="5396992326001">
+  <param name="playerKey" value="AQ~~,AAACqQLjXwE~,Nq9Z9rk3F20GNgDbGLkEDHbwMyFJR1vx">
+  <param name="isVid" value="true">
+  <param name="isUI" value="true">
+  <param name="dynamicStreaming" value="true">
+  <param name="autoStart" value="false">
+  <param name="includeAPI" value="true">
+  <param name="@videoPlayer" value="5396992326001">
+  <param name="templateLoadHandler" value="onTemplateLoaded">
+  <param name="htmlFallback" value="true">
+  <param name="secureConnections" value="true">
+  <param name="secureHTMLConnections" value="true">
+  </object>  
+  <script type="text/javascript">brightcove.createExperiences();</script>
+</div>
+
+####Key points:
+'
+    expect(described_class.new(data).replace!).to eq \
+    "you complete the module.\n\n<!--video-->\n<div class=\"embed-responsive embed-responsive-16by9\">\n  brightcove-default(5396992326001)  \n  \n</div>\n\n####Key points:\n"
   end
 end
