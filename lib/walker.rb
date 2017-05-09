9433b3d40903a8188ed9129451c6dabbb9800567
Szymon Miedziejko <szymon@mastered.com>
2017-05-09 11:44:42 +0100

    WIP

--- lib/walker.rb
+++ lib/walker.rb
@@ -4,8 +4,9 @@ class Walker < String
               (<param\s+name="playerID"\s+value="(\d+)">).+<\/object>/ixm
     regexp2 = /<script.+brightcove\..+<\/script>/im
 
-    gsub!(regexp1, 'brightcove-default(\2)')
+    gsub!(regexp1, 'brightcove-public(\2)')
     gsub!(regexp2, "")
+    gsub!(/\s*[\n]\s*[\n]<\/div>/, "\n<\/div>")
     fix_css
   end
 
@@ -15,3 +16,22 @@ class Walker < String
     gsub(/video-embed-container/, "embed-responsive embed-responsive-16by9")
   end
 end
+
+# tracker = "<object class=\"BrightcoveExperience video-player\""
+# counter = 0
+# Lesson.where("article LIKE ?", "%#{tracker}%").find_each do |lesson|
+#   article = lesson.article
+#   next unless article
+#   article = Walker.new(article).replace!
+
+#   if article != lesson.article
+#     counter = counter + 1
+#     lesson.update_column(:article, article)
+#   end
+# end
+
+# puts "#{counter} video embeds converted!"
+
+# l = Lesson.find(3337)
+# article = Walker.new(l.article).replace!
+# l.update_column(:article, article)
